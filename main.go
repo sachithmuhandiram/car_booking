2b6cd2eb477d8ddee66bf8cc34f3fe28445e18b8
Sachith Muhandiram <sachith@vizuamatix.com>
2019-08-07 21:55:55 +0530

    last_login date time is differnt from now time

--- main.go
+++ main.go
@@ -48,12 +48,12 @@ func userLoginView(reswt http.ResponseWriter, req *http.Request) {
 	/*
 		Here I am using a custom cookie and its used before user login. This is add purely for learning purposes.
 	*/
-	user_login_cookie := userlogin.PasswordHashing([]byte("test_value")) // this must change to random later
-	userlogin.InitialToken(user_login_cookie)
+	userLoginCookie := userlogin.PasswordHashing([]byte("test_value")) // this must change to random later
+	userlogin.InitialToken(userLoginCookie)
 
 	http.SetCookie(reswt, &http.Cookie{
 		Name:  "login-cookie",
-		Value: user_login_cookie,
+		Value: userLoginCookie,
 		Path:  "/",
 	})
 
@@ -62,15 +62,15 @@ func userLoginView(reswt http.ResponseWriter, req *http.Request) {
 	OutputHTML(reswt, "ui/login.html", nil)
 }
 
-func getEmailView(register_response http.ResponseWriter, register_request *http.Request) {
-	OutputHTML(register_response, "ui/send_verification_email.html", nil)
+func getEmailView(registerResponse http.ResponseWriter, registerRequest *http.Request) {
+	OutputHTML(registerResponse, "ui/send_verification_email.html", nil)
 }
 
-func userHomeView(home_response http.ResponseWriter, home_request *http.Request) {
-	OutputHTML(home_response, "ui/user_home.html", nil)
+func userHomeView(homeResponse http.ResponseWriter, homeRequest *http.Request) {
+	OutputHTML(homeResponse, "ui/user_home.html", nil)
 }
 
-// output html view generic
+// OutputHTML view generic
 func OutputHTML(w http.ResponseWriter, filename string, data interface{}) {
 	t, err := template.ParseFiles(filename)
 	if err != nil {
--- packages/cookiecheck/cookiecheck.go
+++ packages/cookiecheck/cookiecheck.go
@@ -1,17 +1,74 @@
 package cookiecheck
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"time"
 )
 
+// UserSession table struct
+type userSession struct {
+	eventID int
+	jwt     string
+	expired int
+}
+
+// database connection
+func dbConn() (db *sql.DB) {
+	db, err := sql.Open("mysql", "root:7890@tcp(127.0.0.1:3306)/car_booking_users")
+
+	if err != nil {
+		log.Println("Can not open database connection")
+	}
+	return db
+}
+
+// CheckCookie fuction
 func CheckCookie(homeRequest *http.Request) bool {
+
+	var userCookies userSession
 	// This function checks whether user has a valid token, if so send to home page or send to login
 	cookie, cookieError := homeRequest.Cookie("user-cookie")
 
 	if cookieError != nil {
+		log.Println("No user-cookie set")
 		return false
 	}
 	log.Println("User cookie there : ", cookie)
+	// update user_session table
+	db := dbConn()
+	defer db.Close()
+
+	//get data from user_session table
+	loginTokenCheck := db.QueryRow("select event_id,expired from user_session where jwt=?", cookie.Value).Scan(&userCookies.eventID, &userCookies.expired)
+
+	if loginTokenCheck != nil {
+		log.Println("Couldnt read from user_session table")
+	}
+
+	if userCookies.expired == 1 {
+		log.Println("User token expired")
+		return false
+	}
+	dt := time.Now()
+
+	dateTime := dt.Format("2006-01-01 15:04:05.99999")
+	//update initial user details table
+	sessionUpdate, sessionErr := db.Prepare("update user_session set last_login=? where event_id=?")
+
+	if sessionErr != nil {
+		log.Println("Couldnt update initial user table")
+	}
+
+	_, updateErr := sessionUpdate.Exec(dateTime, userCookies.eventID)
+
+	if updateErr != nil {
+		log.Println("Couldnt execute initial update")
+
+	}
+	log.Printf("Initial table updated for event id %d : ", userCookies.eventID)
+
+	// log.Println(userSessioneventID)
 	return true
 }
