e30134e660670ce1e687570ca44e9370ad67ce43
Sachith Muhandiram <sachith@vizuamatix.com>
2019-08-07 14:09:47 +0530

    changed variable names into golang standard

--- packages/userlogin/userlogin.go
+++ packages/userlogin/userlogin.go
@@ -18,10 +18,10 @@ type users struct {
 	password string //`json:"password"`
 }
 
-type user_login_struct struct {
-	event_id int
-	token    string
-	used     bool
+type userLoginStruct struct {
+	eventID int
+	token   string
+	used    bool
 }
 
 // database connection
@@ -34,8 +34,8 @@ func dbConn() (db *sql.DB) {
 	return db
 }
 
-// initial token insert
-func InitialToken(user_login_cookie string) {
+// InitialToken insert to initial login page
+func InitialToken(userLoginCookie string) {
 	db := dbConn()
 	defer db.Close()
 
@@ -43,60 +43,60 @@ func InitialToken(user_login_cookie string) {
 
 	dateTime := dt.Format("2006-01-01 15:04:05.99999")
 
-	token_insert, tp_error := db.Prepare("insert into user_initial_login (token,created_date) values(?,?)")
+	tokenInsert, tpError := db.Prepare("insert into user_initial_login (token,created_date) values(?,?)")
 
-	if tp_error != nil {
+	if tpError != nil {
 		log.Println("Error prepairing initial token to table")
 	}
 
-	in_res, in_err := token_insert.Exec(user_login_cookie, dateTime)
+	inRes, inErr := tokenInsert.Exec(userLoginCookie, dateTime)
 
-	if in_err != nil {
-		log.Println("Coulnd insert data to table", in_res)
-		panic(in_err.Error())
+	if inErr != nil {
+		log.Println("Coulnd insert data to table", inRes)
+		panic(inErr.Error())
 	}
-	log.Println("Initial Token inserted ", user_login_cookie)
+	log.Println("Initial Token inserted ", userLoginCookie)
 
-	defer token_insert.Close()
+	defer tokenInsert.Close()
 
 }
 
-func UserLoginData(login_response http.ResponseWriter, login_request *http.Request) {
+func UserLoginData(loginResponse http.ResponseWriter, loginRequest *http.Request) {
 
 	var userSessioneventID int
 
-	if login_request.Method != "POST" {
+	if loginRequest.Method != "POST" {
 		log.Panic("Form data is not Post")
-		http.Redirect(login_response, login_request, "/", http.StatusSeeOther)
+		http.Redirect(loginResponse, loginRequest, "/", http.StatusSeeOther)
 	}
 
-	cookie, cookie_error := login_request.Cookie("login-cookie") // returns cookie or an error
+	cookie, cookieError := loginRequest.Cookie("login-cookie") // returns cookie or an error
 
 	// check incoming cookie with db
 
-	if cookie_error != nil {
+	if cookieError != nil {
 		log.Fatal("Cookies dont match")
 	} else {
 		log.Println("Got cookie : ", cookie)
-		userName := login_request.FormValue("username")
-		password := login_request.FormValue("password")
-		remember_me := login_request.FormValue("remember_me")
-		fmt.Println("Rember me  : ", remember_me)
+		userName := loginRequest.FormValue("username")
+		password := loginRequest.FormValue("password")
+		rememberMe := loginRequest.FormValue("remember_me")
+		fmt.Println("Rember me  : ", rememberMe)
 
-		user_login, eventID := userLoginProcessing(userName, password, cookie.Value)
+		userLogin, eventID := userLoginProcessing(userName, password, cookie.Value)
 
-		if user_login {
+		if userLogin {
 			/*
 				Password matches, insert jwt and details to user_session table.
 				Update initial loging table setting used=1 and next event id
 			*/
-			jwt, jwt_err := GenerateJWT(cookie.Value, 30) // for now its 30min session
+			jwt, jwtErr := GenerateJWT(cookie.Value, 30) // for now its 30min session
 
-			if jwt_err != nil {
-				log.Println("Can not generate jwt token", jwt_err)
+			if jwtErr != nil {
+				log.Println("Can not generate jwt token", jwtErr)
 			}
 
-			http.SetCookie(login_response, &http.Cookie{
+			http.SetCookie(loginResponse, &http.Cookie{
 				Name:  "user-cookie",
 				Value: jwt,
 				Path:  "/home",
@@ -155,10 +155,10 @@ func UserLoginData(login_response http.ResponseWriter, login_request *http.Reque
 
 			log.Println(eventID)
 
-			http.Redirect(login_response, login_request, "/home", http.StatusSeeOther)
+			http.Redirect(loginResponse, loginRequest, "/home", http.StatusSeeOther)
 		} else {
 			// This is where I need to modify not to generate new token for login
-			http.Redirect(login_response, login_request, "/", http.StatusSeeOther)
+			http.Redirect(loginResponse, loginRequest, "/", http.StatusSeeOther)
 		}
 	}
 
@@ -167,53 +167,53 @@ func UserLoginData(login_response http.ResponseWriter, login_request *http.Reque
 }
 
 // user data processing functions
-func userLoginProcessing(user_name string, password string, cookie string) (bool, int) {
-	var login_user users
-	var user_initial user_login_struct
+func userLoginProcessing(userName string, password string, cookie string) (bool, int) {
+	var loginUser users
+	var userInitial userLoginStruct
 
 	db := dbConn()
 
 	defer db.Close()
 
 	// login page defined token checking
-	login_token_check := db.QueryRow("SELECT event_id,used FROM user_initial_login WHERE token=? and used=0", cookie).Scan(&user_initial.event_id, &user_initial.used)
+	loginTokenCheck := db.QueryRow("SELECT event_id,used FROM user_initial_login WHERE token=? and used=0", cookie).Scan(&userInitial.eventID, &userInitial.used)
 
-	if login_token_check != nil {
+	if loginTokenCheck != nil {
 		log.Println("user_initial_login table read faild") // posible system error or hacking attempt ?
-		log.Println(login_token_check)
+		log.Println(loginTokenCheck)
 		return false, 0
 	}
 
 	// update initial user details table
-	initial_update, init_err := db.Prepare("update user_initial_login set used=1 where event_id=?")
+	initialUpdate, initErr := db.Prepare("update user_initial_login set used=1 where event_id=?")
 
-	if init_err != nil {
+	if initErr != nil {
 		log.Println("Couldnt update initial user table")
 		return false, 0 // we shouldnt compare password
 	}
 
-	_, update_err := initial_update.Exec(user_initial.event_id)
+	_, updateErr := initialUpdate.Exec(userInitial.eventID)
 
-	if update_err != nil {
+	if updateErr != nil {
 		log.Println("Couldnt execute initial update")
 
 	}
-	log.Printf("Initial table updated for event id %d : ", user_initial.event_id)
+	log.Printf("Initial table updated for event id %d : ", userInitial.eventID)
 	// end login page token checking
 
-	read_error := db.QueryRow("SELECT id,password FROM car_booking_users WHERE username=?", user_name).Scan(&login_user.id, &login_user.password)
+	readError := db.QueryRow("SELECT id,password FROM car_booking_users WHERE username=?", userName).Scan(&loginUser.id, &loginUser.password)
 	defer db.Close()
-	if read_error != nil {
+	if readError != nil {
 		//http.Redirect(res, req, "/", 301)
 		log.Println("data can not be taken")
 
 	}
 
-	compare_password := bcrypt.CompareHashAndPassword([]byte(login_user.password), []byte(password))
+	comparePassword := bcrypt.CompareHashAndPassword([]byte(loginUser.password), []byte(password))
 
 	// https://stackoverflow.com/questions/52121168/bcrypt-encryption-different-every-time-with-same-input
 
-	if compare_password != nil {
+	if comparePassword != nil {
 		/*
 			Here I need to find a way to make sure that initial token is not get created each time wrong username password
 
@@ -224,40 +224,39 @@ func userLoginProcessing(user_name string, password string, cookie string) (bool
 	} //else {
 
 	log.Println("Hurray")
-	return true, user_initial.event_id
+	return true, userInitial.eventID
 	//}
 
 }
 
-// internal functions
+// PasswordHashing exports hased password
 func PasswordHashing(pass []byte) string {
 
-	hashed_pass, err := bcrypt.GenerateFromPassword(pass, 8)
+	hashedPass, err := bcrypt.GenerateFromPassword(pass, 8)
 	if err != nil {
 		log.Println(err)
 	}
-	return string(hashed_pass)
+	return string(hashedPass)
 }
 
-/*
-	This will generate a JWT for a user seassion. Here I use initially generate token as an input.
-	Also the time needs to keep token (valid_duration).
-*/
-func GenerateJWT(initial_token string, valid_duration int) (string, error) {
+//	GenerateJWT will generate a JWT for a user seassion. Here I use initially generate token as an input.
+//	Also the time needs to keep token (valid_duration).
 
-	login_key := []byte(initial_token)
+func GenerateJWT(initialToken string, validDuration int) (string, error) {
+
+	loginKey := []byte(initialToken)
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
 
 	claims["authorized"] = true
-	claims["exp"] = time.Now().Add(time.Minute * time.Duration(valid_duration))
+	claims["exp"] = time.Now().Add(time.Minute * time.Duration(validDuration))
 
-	jwt_token, jwt_err := token.SignedString(login_key)
+	jwtToken, jwtErr := token.SignedString(loginKey)
 
-	if jwt_err != nil {
-		log.Println("Error creating jwt Token : ", jwt_err)
-		return "", jwt_err
+	if jwtErr != nil {
+		log.Println("Error creating jwt Token : ", jwtErr)
+		return "", jwtErr
 	}
 
-	return jwt_token, nil
+	return jwtToken, nil
 }
