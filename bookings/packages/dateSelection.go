c4348616bc34fac56858f90d7c96efd7181a6e06
Sachith Muhandiram <sachith@vizuamatix.com>
2019-09-04 06:29:09 +0530

    Booking check initial works completed, but data comparision and getting all available vehicles to be done

--- bookings/packages/dateSelection.go
+++ bookings/packages/dateSelection.go
@@ -1,10 +1,30 @@
 package booking
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"text/template"
+	"time"
 )
+
+type booking struct {
+	userID    int
+	vehicleID int
+	startDate time.Time
+	endDate   time.Time
+}
+
+// database connection
+func dbConn() (db *sql.DB) {
+	db, err := sql.Open("mysql", "root:7890@tcp(127.0.0.1:3306)/car_booking")
+
+	if err != nil {
+		log.Println("Can not open database connection")
+	}
+	return db
+}
 
 // ShowDates shows date selection page.
 func ShowDates(res http.ResponseWriter, req *http.Request) {
@@ -13,7 +33,11 @@ func ShowDates(res http.ResponseWriter, req *http.Request) {
 
 // SearchDates gets data from form and parse
 func SearchDates(res http.ResponseWriter, req *http.Request) {
-	log.Println("Sesrch date function")
+	db := dbConn()
+	defer db.Close()
+
+	var booking booking
+
 	if req.Method != "POST" {
 		log.Panic("Search date data is not Post")
 		http.Redirect(res, req, "/dateselection", http.StatusSeeOther)
@@ -26,6 +50,15 @@ func SearchDates(res http.ResponseWriter, req *http.Request) {
 	startDate := req.FormValue("startdate")
 	endDate := req.FormValue("enddate")
 	vehicleType := req.FormValue("vtype")
+
+	t := time.Now()
+	today := fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
+
+	bookingCheck := db.QueryRow("SELECT vehicle_id FROM booking WHERE start_date=? < ? and end_date< ?", startDate, today, endDate).Scan(&booking.vehicleID)
+
+	if bookingCheck != nil {
+		log.Println("Couldnt fetch available vehicles from booking table")
+	}
 
 	log.Println("Start date : ", startDate)
 	log.Println("End date :", endDate)
